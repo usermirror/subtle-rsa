5a7d380fb369a888e42b0c720ec3f8015689dd36
Fouad Matin <m@fouad.org>
2019-02-01 18:44:56 -0800

    init: repo

---
+++ readme.md
@@ -0,0 +1,51 @@
+<br/>
+<p align="center">
+  <strong><code>🤫 subtle-rsa</code></strong>
+</p>
+
+<p align="center">
+  Simple wrapper around <br/>
+  SubtleCrypto RSA implementation.
+</p>
+<br/>
+
+<p align="center">
+  <a href="https://unpkg.com/subtle-rsa/lib/index.js"><img src="https://img.badgesize.io/https://unpkg.com/subtle-rsa/lib/index.js?compression=gzip&amp;label=subtle--rsa"></a>
+  <a href="https://www.npmjs.com/package/subtle-rsa"><img src="https://img.shields.io/npm/v/subtle-rsa.svg?maxAge=3600&label=subtle-rsa&colorB=007ec6"></a>
+</p>
+<br/>
+
+```javascript
+import SubtleRSA from 'subtle-rsa'
+
+let rsa = new SubtleRSA()
+
+let encryptedValue = await rsa.encrypt('value')
+
+if ('value' === (await rsa.decrypt(encryptedValue))) {
+  // success!
+}
+```
+
+#### Installation
+
+Install with npm:
+
+```console
+npm install --save subtle-rsa
+```
+
+Or yarn:
+
+```console
+yarn add subtle-rsa
+```
+
+Or using a script tag:
+
+```html
+<script
+  src="https://unpkg.com/subtle-rsa@^0.1/lib/index.umd.js"
+  crossorigin="anonymous"
+></script>
+```
---
+++ src/index.ts
@@ -0,0 +1,92 @@
+type SubtleRSAInput = {
+  generate?: boolean
+  publicKey?: string
+  privateKey?: string
+}
+
+class SubtleRSA {
+  _publicKey: CryptoKey
+  _privateKey: CryptoKey
+
+  constructor(input: SubtleRSAInput) {
+    if (input.publicKey) {
+      this.importPublicKey(input.publicKey).then(publicKey => {
+        this._publicKey = publicKey
+      })
+    }
+
+    if (input.generate) {
+      this.generateKeys()
+    }
+  }
+
+  encrypt = async (valueToEncrypt: any) => {
+    if (!this._publicKey) {
+      console.warn('SubtleRSA.encrypt(): missing keys: generating')
+      await this.generateKeys()
+    }
+
+    let serializedValue = toArrayBuffer(
+      window.btoa(encodeURIComponent(JSON.stringify(valueToEncrypt)))
+    )
+
+    window.crypto.subtle.encrypt(
+      { name: 'RSA-OAEP' },
+      this._publicKey,
+      serializedValue
+    )
+  }
+
+  importPublicKey = async (jwk: string) => {
+    return await window.crypto.subtle.importKey(
+      'jwk',
+      {
+        n: jwk,
+        e: 'AQAB',
+        ext: true,
+        kty: 'RSA',
+        alg: 'RSA-OAEP-256'
+      },
+      {
+        name: 'RSA-OAEP',
+        hash: { name: 'SHA-256' }
+      },
+      false,
+      ['encrypt']
+    )
+  }
+
+  generateKeys = async () => {
+    let keyMaterials = await window.crypto.subtle.generateKey(
+      {
+        name: 'RSA-OAEP',
+        modulusLength: 2048,
+        hash: { name: 'SHA-256' },
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01])
+      },
+      false,
+      ['encrypt', 'decrypt']
+    )
+
+    this._privateKey = keyMaterials.privateKey
+    this._publicKey = keyMaterials.publicKey
+  }
+}
+
+function toArrayBuffer(str: string) {
+  // check if it's already an array buffer
+  if (typeof str !== 'string') {
+    return str
+  }
+
+  const buf = new ArrayBuffer(str.length * 2)
+  const bufUint16 = new Uint16Array(buf)
+
+  for (let i = 0; i < str.length; i++) {
+    bufUint16[i] = str.charCodeAt(i)
+  }
+
+  return buf
+}
+
+export default SubtleRSA
